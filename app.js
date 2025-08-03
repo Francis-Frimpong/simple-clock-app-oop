47f15d9cbc930c27b3aeedd90c2aa602056f21de
Francis <brakay1136@gmail.com>
2025-08-03 15:17:24 -0700

    digital clock oop version

--- app.js
+++ app.js
@@ -21,8 +21,3 @@ const clock = document.getElementById("clock");
 const digitalClock = new DigitalClock(clock);
 digitalClock.updateClock();
 digitalClock.start();
-// let hr = 0 + "0";
-// let min = 0 + "0";
-// let sec = 0 + "0";
-// console.log(hr);
-// clock.textContent = `${hr}:${min}:${sec}`;
